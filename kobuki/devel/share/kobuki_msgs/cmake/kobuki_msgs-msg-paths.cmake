3470adb990399645ff62f45bbe3b7ee157613244
Chris Card <ccard@mymail.mines.edu>
2014-10-20 09:44:11 -0600

    added kobuki files

---
+++ kobuki/devel/share/kobuki_msgs/cmake/kobuki_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(kobuki_msgs_MSG_INCLUDE_DIRS "/home/chris/Documents/mines/mines_grad/csci598/hcr_term_project/kobuki/src/kobuki_msgs/msg;/home/chris/Documents/mines/mines_grad/csci598/hcr_term_project/kobuki/devel/share/kobuki_msgs/msg")
+set(kobuki_msgs_MSG_DEPENDENCIES std_msgs;actionlib_msgs)
