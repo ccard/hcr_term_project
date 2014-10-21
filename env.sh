3c6772a8fa3bd428e76027326a8c4680fd04b4b4
Chris Card <ccard@mymail.mines.edu>
2014-10-21 16:06:14 -0600

    fixed spelling error

--- env.sh
+++ env.sh
@@ -5,4 +5,4 @@ source /rocon/devel/setup.bash
 cd kobuki/; catkin_make; cd ..
 source /kobuki/devel/setup.bash
 cd turtlebot/; catkin_make; cd ..
-source /turtle/devel/setup.bash
+source /turtlebot/devel/setup.bash
