4d75de5a8b81eecf68f6faade9ad53a72185a708
Chris Card <ccard@mymail.mines.edu>
2014-11-12 15:55:27 -0700

    update the env to compile our slam code

--- env.sh
+++ env.sh
@@ -4,7 +4,12 @@ echo "See .env.log for any potential errors"
 
 cd rocon/; catkin_make; cd .. # >> .env.log
 source ./rocon/devel/setup.bash # >> .env.log
+
 cd kobuki/; catkin_make; cd .. #>> .env.log
 source ./kobuki/devel/setup.bash #>> .env.log
+
 cd turtlebot/; catkin_make; cd .. # >> .env.log
 source ./turtlebot/devel/setup.bash # >> .env.log
+
+cd slam_eval_code; catkin_make; cd ..
+source ./slam_eval_code/devel/setup.bash
